b82abf1ef28ada5219ef5673c98bf58e73a5b917
Aditya vennam <23pa1a12n2@vishnu.edu.in>
2026-08-05 01:13:12 +0530

    Update 02_app.py

--- 02_app.py
+++ 02_app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from dotenv import load_dotenv
 from rag_engine import RAGEngine
@@ -19,6 +19,9 @@ except ValueError as e:
     rag_engine = None
 
 # Register API routes and pass the rag_engine instance
+@app.route("/")
+def home():
+    return render_template("index.html")
 register_routes(app, rag_engine)
 
 if __name__ == '__main__':
